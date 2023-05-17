7351b9c08baea46b832c771466f2d394fe0d34fd
HazemEl-masry <hazemelmasryofficial@gmail.com>
2023-05-18 01:22:10 +0300

    make http call

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,5 +1,9 @@
+import 'package:http/http.dart' as http;
 class WeatherServices
 {
-  void getWeather({required String cityName})
-  {}
+  void getWeather({required String cityName}) async
+  {
+    Uri url = Uri.parse("http://api.weatherapi.com/v1/current.json?key=52f93cd6b6ac421790b121934231705&q=London&aqi=no");
+    http.Response response = await http.get(url);
+  }
 }
