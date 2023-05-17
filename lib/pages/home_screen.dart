2dc8b5a5b504306f082bf544ce01475a51a0e3ae
HazemEl-masry <hazemelmasryofficial@gmail.com>
2023-05-17 16:44:18 +0300

    home screen

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -5,6 +5,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            "Weather App",
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.w700
+          ),
+        ),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "There is no weather 😔 Start",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "searching now 🔎",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
