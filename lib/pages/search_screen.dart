829ebbe7b6573a989a0837393072a708ae245a58
HazemEl-masry <hazemelmasryofficial@gmail.com>
2023-05-20 22:45:50 +0300

    test api services

--- lib/pages/search_screen.dart
+++ lib/pages/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/services/weather_services.dart';
 
 class SearchPage extends StatelessWidget {
 
@@ -23,6 +24,8 @@ class SearchPage extends StatelessWidget {
             onSubmitted: (value)
             {
               cityName = value;
+              WeatherServices services = WeatherServices();
+              services.getWeather(cityName: cityName!);
             },
             decoration: InputDecoration(
               contentPadding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 20.0),
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -6,9 +6,11 @@ class WeatherServices
   void getWeather({required String cityName}) async
   {
     String baseUrl = "http://api.weatherapi.com/v1";
-    String apiKey = "52f93cd6b6ac421790b121934231705";
-    Uri url = Uri.parse("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=7");
+    String apiKey = "0f06e9aa4dbc4ec68bb174950231805";
+    Uri url = Uri.parse("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=3");
     http.Response response = await http.get(url);
     Map<String , dynamic> data = jsonDecode(response.body);
+
+    print(data);
   }
 }
